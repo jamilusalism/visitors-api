808651ee8724f4240d5d887ef9198c5af8ddbfe7
Jamilu Salisu <jamilusalis@gmail.com>
2023-09-09 16:06:35 +0100

    init

--- app.py
+++ app.py
@@ -9,257 +9,188 @@ db = SQLAlchemy()
 app = Flask(__name__)
 ma = Marshmallow(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///donation.db"
-app.config['SECRET_KEY'] = 'help_people_in_need'
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///exchange_wallet.db"
+app.config['SECRET_KEY'] = 'swapped_funds'
 db.init_app(app)
 
 
-class Hospital(db.Model):
-    __tablename__ = 'hospitals'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    phone = db.Column(db.String, nullable=False, unique=True)
-    address = db.Column(db.String, nullable=False)
-    bank_name = db.Column(db.String, nullable=False)
-    account_number = db.Column(db.String, nullable=False)
-    account_name = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=True)
-
-
-class Donor(db.Model):
-    __tablename__ = 'donors'
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     phone = db.Column(db.String, nullable=False, unique=True)
-    address = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=timestamp)
     updated_at = db.Column(db.DateTime, nullable=True)
 
 
-class Patient(db.Model):
-    __tablename__ = 'patients'
+class USDWallet(db.Model):
+    __tablename__ = 'usd_wallet'
     id = db.Column(db.Integer, primary_key=True)
-    patient_id = db.Column(db.String, nullable=False, unique=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False, unique=True)
-    phone = db.Column(db.String, nullable=False, unique=True)
-    address = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=True)
+    dollar_balance = db.Column(db.Float(precision=2), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=timestamp)
 
 
-class PatientRecord(db.Model):
-    __tablename__ = 'patient_records'
+class NairaWallet(db.Model):
+    __tablename__ = 'naira_wallet'
     id = db.Column(db.Integer, primary_key=True)
-    patient_id = db.Column(db.String, db.ForeignKey(
-        'patients.id'), nullable=False)
-    hospital_id = db.Column(db.Integer, db.ForeignKey(
-        'hospitals.id'), nullable=False)
-    hospital = db.relationship("Hospital")
-    sickness = db.Column(db.String, nullable=False)
-    amount = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=True)
+    naira_balance = db.Column(db.Float(precision=2), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=timestamp)
 
 
-class Donate(db.Model):
-    __tablename__ = 'funds_donated'
-    id = db.Column(db.Integer, primary_key=True)
-    donor_id = db.Column(db.Integer, db.ForeignKey(
-        'donors.id'), nullable=False)
-    record_id = db.Column(db.Integer, db.ForeignKey(
-        'patient_records.id'), nullable=False)
-    amount_donating = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=True)
-
-
-class HospitalSchema(ma.SQLAlchemyAutoSchema):
+class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Hospital
+        model = User
 
 
-class DonorSchema(ma.SQLAlchemyAutoSchema):
+class USDWalletSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Donor
+        model = USDWallet
 
 
-class PatientSchema(ma.SQLAlchemyAutoSchema):
+class NairaWalletSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Patient
-
-
-class PatientRecordSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = PatientRecord
-        include_relationships = True
-        include_fk = True
-    hospital = ma.Nested(HospitalSchema)
-
-
-class DonateSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Donate
+        model = NairaWallet
 
 
 with app.app_context():
     db.create_all()
 
+exchange_rate = float(450)
+
 
 @app.route("/")
-def hello_world():
-    return "Donation running at " + str(timestamp)
-
-# Create New Endpoints
+def home():
+    return "Exchange running at " + str(timestamp)
 
 
-@app.post("/create/hospital")
-def create_hospital():
-    hospital = Hospital(
-        name=request.json['name'],
-        phone=request.json['phone'],
-        address=request.json['address'],
-        bank_name=request.json['bank_name'],
-        account_number=request.json['account_number'],
-        account_name=request.json['account_name'],
-        created_at=timestamp
-    )
-    user_phone = Hospital.query.filter_by(phone=hospital.phone).first()
-    if user_phone:
-        return {"message": "Hospital already exist"}, 400
+@app.post("/register")
+def register():
+    name = request.json['name']
+    email = request.json['email']
+    phone = request.json['phone']
+    password = request.json['password']
 
-    db.session.add(hospital)
+    check_phone = User.query.filter_by(phone=phone).first()
+    check_email = User.query.filter_by(email=email).first()
+    if check_phone or check_email:
+        return {"message": "Phone number or email already exists"}, 400
+
+    new_user = User(name=name, email=email, phone=phone, password=password)
+    db.session.add(new_user)
     db.session.commit()
-    return {"message": "Hospital created successfully"}, 200
+    return {"message": "User created successfully"}
 
 
-@app.post("/create/donor")
-def create_donor():
-    donor = Donor(
-        name=request.json['name'],
-        email=request.json['email'],
-        phone=request.json['phone'],
-        address=request.json['address'],
-        created_at=timestamp
-    )
-    user_phone = Donor.query.filter_by(phone=donor.phone).first()
-    user_email = Donor.query.filter_by(email=donor.email).first()
-
-    if user_phone or user_email:
-        return {"message": "Donor already exists"}, 400
-
-    db.session.add(donor)
-    db.session.commit()
-    return {"message": "Donor created successfully"}, 200
+@app.get("/users")
+def get_users():
+    users = User.query.all()
+    users = UserSchema().dump(users, many=True)
+    return {"users": users}
 
 
-@app.post("/create/patient")
-def create_patient():
-    patient = Patient(
-        patient_id=request.json['patient_id'],
-        name=request.json['name'],
-        email=request.json['email'],
-        phone=request.json['phone'],
-        address=request.json['address'],
-        created_at=timestamp
-    )
-    user_phone = Patient.query.filter_by(phone=patient.phone).first()
-    user_email = Patient.query.filter_by(email=patient.email).first()
-    user_id = Patient.query.filter_by(patient_id=patient.patient_id).first()
-
-    if user_phone or user_email or user_id:
-        return {"message": "Patient already exists"}, 400
-
-    db.session.add(patient)
-    db.session.commit()
-    return {"message": "Patient created successfully"}, 200
+@app.post('/desposit/usd')
+def deposit_usd():
+    amount = request.json['amount']
+    try:
+        amount = float(amount)
+        new_deposit = USDWallet(dollar_balance=amount)
+        db.session.add(new_deposit)
+        db.session.commit()
+        return {"message": "Deposit successful"}, 200
+    except ValueError:
+        return {"message": "Please enter a valid amount"}, 400
 
 
-@app.post("/create/record")
-def create_record():
-    record = PatientRecord(
-        patient_id=request.json['patient_id'],
-        hospital_id=request.json['hospital_id'],
-        sickness=request.json['sickness'],
-        amount=request.json['amount'],
-        created_at=timestamp
-    )
-
-    db.session.add(record)
-    db.session.commit()
-    return {"message": "Record created successfully"}, 200
+@app.post('/desposit/ngn')
+def deposit_ngn():
+    amount = request.json['amount']
+    try:
+        amount = float(amount)
+        new_deposit = NairaWallet(naira_balance=amount)
+        db.session.add(new_deposit)
+        db.session.commit()
+        return {"message": "Deposit successful"}, 200
+    except ValueError:
+        return {"message": "Please enter a valid amount"}, 400
 
 
-@app.post("/create/donate")
-def create_donate():
-    donate = Donate(
-        donor_id=request.json['donor_id'],
-        record_id=request.json['record_id'],
-        amount_donating=request.json['amount_donating'],
-        created_at=timestamp
-    )
-
-    db.session.add(donate)
-    db.session.commit()
-    return {"message": "Donation created successfully"}, 200
+@app.get("/balance/usd/<id>")
+def get_usd_balance(id):
+    usd_balance = USDWallet.query.filter_by(id=id).first()
+    if usd_balance:
+        usd_balance = USDWalletSchema().dump(usd_balance)
+        return {"usd_balance": usd_balance}, 200
+    else:
+        return {"message": "USD record does not exist"}, 400
 
 
-# GET Endpoints
-@app.get("/list/hospital")
-def list_hospital():
-    hospitals = Hospital.query.all()
-    hospitals = HospitalSchema().dump(hospitals, many=True)
-    return {"hospitals": hospitals}
+@ app.get("/balance/ngn/<id>")
+def get_ngn_balance(id):
+    ngn_balance = NairaWallet.query.filter_by(id=id).first()
+    if ngn_balance:
+        ngn_balance = NairaWalletSchema().dump(ngn_balance)
+        return {"ngn_balance": ngn_balance}, 200
+    else:
+        return {"message": "NGN record does not exist"}, 400
 
 
-@app.get("/list/donor")
-def list_donor():
-    donors = Donor.query.all()
-    donors = DonorSchema().dump(donors, many=True)
-    return {"donors": donors}
+@app.get('/history/usd')
+def get_usd_history():
+    usd_history = USDWallet.query.all()
+    usd_history = USDWalletSchema().dump(usd_history, many=True)
+    return {"usd_history": usd_history}
 
 
-@app.get("/list/patient")
-def list_patient():
-    patients = Patient.query.all()
-    patients = PatientSchema().dump(patients, many=True)
-    return {"patients": patients}
+@app.get('/history/ngn')
+def get_ngn_history():
+    ngn_history = NairaWallet.query.all()
+    ngn_history = NairaWalletSchema().dump(ngn_history, many=True)
+    return {"ngn_history": ngn_history}
 
 
-@app.get("/list/records")
-def list_records():
-    records = PatientRecord.query.all()
-    records = PatientRecordSchema().dump(records, many=True)
-    return {"records": records}
+@app.patch("/swap")
+def swap():
+    naira_wallet_id = request.json['naira_wallet_id']
+    usd_wallet_id = request.json['usd_wallet_id']
+    to_currency = request.json['to_currency']
+    amount = request.json['amount']
+    try:
+        amount = float(amount)
+        naira_wallet = NairaWallet.query.filter_by(id=naira_wallet_id).first()
+        usd_wallet = USDWallet.query.filter_by(id=usd_wallet_id).first()
 
+        if not naira_wallet:
+            return {"message": "NGN wallet could not be found"}, 400
 
-@app.get("/list/transactions")
-def list_transactions():
-    transactions = Donate.query.all()
-    transactions = DonateSchema().dump(transactions, many=True)
-    return {"transactions": transactions}
+        if not usd_wallet:
+            return {"message": "USD wallet could not be found"}, 400
 
+        if to_currency == "USD":
+            # check if naira wallet has enough funds
+            if naira_wallet.naira_balance < amount:
+                return {"message": "Insufficient NGN funds"}, 400
 
-@app.get("/view/donate/<record_id>")
-def view_donate(record_id):
-    donations = Donate.query.filter_by(record_id=record_id).all()
-    donations = DonateSchema().dump(donations, many=True)
-    return {"donations": donations}
+            # deduct exact naira amount and topup to usd wallet
+            naira_wallet.naira_balance -= amount
+            usd_wallet.dollar_balance += (amount / exchange_rate)
+            db.session.commit()
+            return {"message": "Swap of NGN{} to USD is successful".format(amount)}
 
+        elif to_currency == "NGN":
+            # check if usd wallet has enough funds
+            if usd_wallet.dollar_balance < amount:
+                return {"message": "Insufficient USD funds"}, 400
 
-# DELETE Endpoints
-@app.delete("/delete/records/<id>")
-def delete_record(id):
-    record = PatientRecord.query.get(id)
-    if not record:
-        return {"message": "Record not found"}, 404
-    db.session.delete(record)
-    db.session.commit()
-    return {"message": "Record deleted successfully"}, 200
+            # deduct exact dollar amount and topup to naira wallet
+            usd_wallet.dollar_balance -= amount
+            naira_wallet.naira_balance += (amount * exchange_rate)
+            db.session.commit()
+            return {"message": "Swap of USD{} to NGN is successful".format(amount)}
 
+        else:
+            return {"message": "Invalid currency"}, 400
 
-if __name__ == "__main__":
-    app.run()
+    except ValueError:
+        return {"message": "Please enter a valid amount"}, 400
